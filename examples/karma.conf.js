4fd70bd89021201e8e350e1dda331b7c8e463439
Jonathan Reátegui <j.reategui2012@gmail.com>
2024-06-16 01:45:18 -0400

    Generation karma junit and htmlfile reports

--- examples/karma.conf.js
+++ examples/karma.conf.js
@@ -10,7 +10,9 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter'),
+      require('karma-htmlfile-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -20,7 +22,15 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'html', 'junit'],
+    junitReporter: {
+      outputDir: 'test-results',
+      outputFile: 'karma-junit-reporter.xml',
+      useBrowserName: false
+    },
+    htmlReporter: {
+      outputFile: 'test-results/karma-htmlfile-reporter.html',
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
